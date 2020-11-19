78102a1e6d93f2333423bcb4faa325a47f72ccac
KhoaHuynh1416 <63137900+KhoaHuynh1416@users.noreply.github.com>
2020-11-19 11:45:20 +0700

    [Add]: QLSanPham DTO + DAO

---
+++ SanPhamDTO.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlysanpham;
+
+/**
+ *
+ * @author PC
+ */
+public class SanPhamDTO {
+    String MaSP;
+    String TenSP;
+    int SoLuong;
+    float DonGia;
+    String MaLoai;
+    String MaNSX;
+
+    public String getMaLoai() {
+        return MaLoai;
+    }
+
+    public void setMaLoai(String MaLoai) {
+        this.MaLoai = MaLoai;
+    }
+
+    public String getMaNSX() {
+        return MaNSX;
+    }
+
+    public void setMaNSX(String MaNSX) {
+        this.MaNSX = MaNSX;
+    }
+
+    public String getMaSP() {
+        return MaSP;
+    }
+
+    public void setMaSP(String MaSP) {
+        this.MaSP = MaSP;
+    }
+
+    public String getTenSP() {
+        return TenSP;
+    }
+
+    public void setTenSP(String TenSP) {
+        this.TenSP = TenSP;
+    }
+
+    public int getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(int SoLuong) {
+        this.SoLuong = SoLuong;
+    }
+
+    public float getDonGia() {
+        return DonGia;
+    }
+
+    public void setDonGia(float DonGia) {
+        this.DonGia = DonGia;
+    }
+}
+                                                                                                                                                          
