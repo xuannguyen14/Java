f90a55bbde06e64f7a34fd8d5bce7ab4c0f3f7b3
mitsunari635 <53365907+mitsunari635@users.noreply.github.com>
2020-12-01 23:51:09 +0700

    Add files via upload

--- PhoneStore/src/BUS/NhanvienBUS.java
+++ PhoneStore/src/BUS/NhanvienBUS.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package BUS;
-
+import DAO.NhanvienDAO;
+import DTO.NhanvienDTO;
+import java.util.ArrayList;
 /**
  *
  * @author pc
  */
 public class NhanvienBUS {
-    
+    public ArrayList<NhanvienDTO> showAll(){
+        return NhanvienDAO.showAll();
+    }
+    public void addnv(NhanvienDTO nv){
+        NhanvienDAO.addnv(nv);
+    }
+    public void editnv(NhanvienDTO nv){
+        NhanvienDAO.editnv(nv);
+    }
+    public void delnv(String manv){
+        NhanvienDAO.delnv(manv);
+    }
+    public ArrayList<NhanvienDTO> findnv(String attri,String manv){
+        return NhanvienDAO.findnv(attri,manv);
+    }
 }
